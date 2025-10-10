6d0f7fe19c0eaa7c82c766e279ca3e3d4225886b
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2025-10-10 06:46:10 +0530

    Update download_and_upload.py

--- download_and_upload.py
+++ download_and_upload.py
@@ -300,8 +300,8 @@ async def automated_posting():
             print(f"Auto-processing: {post_url}")
             await process_url(post_url)
             # Sleep for 1-1.5 hours
-         #   sleep_time = random.randint(3600, 4500)
-            sleep_time = random.randint(3600, 5400)
+            sleep_time = random.randint(60, 90)
+            #sleep_time = random.randint(3600, 5400)
             print(f"Sleeping for {sleep_time} seconds")
             await asyncio.sleep(sleep_time)
         except Exception as e:
